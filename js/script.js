065061cb86baa89efd6b80c63bb1a01485c90225
Fabio Mazzarella <fabiomazzarella97@hotmail.com>
2021-10-21 23:58:26 -0300

    Desktop version

--- js/script.js
+++ js/script.js
@@ -1,40 +1,74 @@
-window.onload=function(){
-    var minusBtn = document.getElementById("minus"),
-        plusBtn = document.getElementById("add-cart"),
-        numberPlace = document.getElementById("numberPlace"),
-        submitBtn = document.getElementById("submit"),
-        number = 0, /// number value
-        min = 0, /// min number
-        max = 30; /// max number
+var minusBtn = document.getElementById("minus"),
+    plusBtn = document.getElementById("plus"),
+    minusBtnJean = document.getElementById("minusJean"),
+    plusBtnJean = document.getElementById("plusJean"),
+    numberPlace = document.getElementById("numberPlace"),
+    numberPlaceJean = document.getElementById("numberPlaceJean"),
+    numberPlaceCart =  document.getElementById("numberPlaceCart");
+    pricePlace = document.getElementById("red-price"),
+    pricePlaceJean = document.getElementById("normal-price"),
+    subtotalPlace = document.getElementById("subtotalPlace")
+    totalPlace = document.getElementById("totalPlace")
+    subtotal=14842
+    total=14842
+    number = 1,
+    numberJean = 1
+    numberCart=2;
+    priceUnity=7421;
+    price = 7421;
+    priceJean = 7421;
+    min = 1
         
     minusBtn.onclick = function(){
-        if (number>min){
-           number = number-1; /// Minus 1 of the number
-           numberPlace.innerText = number ; /// Display the value in place of the number
-        
-        }
-        if(number == min) {        
-            numberPlace.style.color= "red";
-            setTimeout(function(){numberPlace.style.color= "black"},500)
-        }
-        else {
-            numberPlace.style.color="black";            
+        console.log('rest');
+        if (number>min ){
+            number = number-1;
+            numberCart = numberCart-1;
+            numberPlace.innerText = number ; /// Display the value in place of the number
+            numberPlaceCart.innerText = numberCart ;
+            price = price - priceUnity;
+            subtotal = subtotal - priceUnity;
+            subtotalPlace.innerText = "$"+subtotal;
+            totalPlace.innerText = "$"+subtotal;
+
         }
     }
     plusBtn.onclick = function(){
-        if(number<max){
-            number = number+1;
-            numberPlace.innerText = number ; /// Display the value in place of the number
-        }     
-        if(number == max){
-                numberPlace.style.color= "red";
-                setTimeout(function(){numberPlace.style.color= "black"},500)
-        }
+        console.log('sum');
+        number = number+1;
+        numberCart = numberCart+1;
+        numberPlace.innerText = number ;
+        numberPlaceCart.innerText = numberCart ;
+        price = price + priceUnity;
+        subtotal = price + priceJean;
+        subtotalPlace.innerText = "$"+subtotal;
+        totalPlace.innerText = "$"+subtotal;
+
+    }     
         
-        else {
-            numberPlace.style.color= "black"          
-        }       
+    minusBtnJean.onclick = function(){
+        console.log('rest');
+        if (numberJean>min){
+            numberJean = numberJean-1;
+            numberCart = numberCart-1;
+            numberPlaceJean.innerText = numberJean ;
+            numberPlaceCart.innerText = numberCart ;
+            priceJean = priceJean - priceUnity;
+            subtotal = subtotal - priceUnity;
+            subtotalPlace.innerText = "$"+subtotal;
+            totalPlace.innerText = "$"+subtotal;
+        }
     }
-    
-    
-}
+    plusBtnJean.onclick = function(){
+        console.log('sum');
+
+        numberJean = numberJean+1;
+        numberCart = numberCart+1;
+        numberPlaceJean.innerText = numberJean ;
+        numberPlaceCart.innerText = numberCart ;
+        priceJean = priceJean + priceUnity;
+        subtotal = price + priceJean;
+        subtotalPlace.innerText = "$"+subtotal;
+        totalPlace.innerText = "$"+subtotal;
+    }     
+
